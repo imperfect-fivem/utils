2becc680de4c3c43099e8763e2a84e90c0a50e63
Imperfect <owner@m-imperfect.net>
2024-08-06 18:00:50 +0300

    Generic Format Update

--- src/server_dependency.lua
+++ src/server_dependency.lua
@@ -1,17 +1,22 @@
+local metadataKeys = { 'server_dependency', 'server_dependencie' --[[server_dependencies without s]] }
 AddEventHandler('onResourceStarting', function (resource)
-    for index = 0, GetNumResourceMetadata(resource, 'server_dependency') do
-        local dependencyResource = GetResourceMetadata(resource, 'server_dependency', index)
-        if not dependencyResource then goto nextIteration end
-        if not GetResourceMetadata(dependencyResource, 'server_only', 0) then
-            print(("^3%s is not a server-only resource, use dependency instead of server_dependency.^7"):format(dependencyResource))
+    for _, metadataKey in ipairs(metadataKeys) do
+        for index = 0, GetNumResourceMetadata(resource, metadataKey) do
+            local dependencyResource = GetResourceMetadata(resource, metadataKey, index)
+            if not dependencyResource then goto nextIteration end
+            local dependencyState = GetResourceState(dependencyResource)
+            if dependencyState == "missing" then
+                print(("Could not find server dependency %s for resource %s."):format(dependencyResource, resource))
+                CancelEvent()
+                goto nextIteration
+            end
+            if not GetResourceMetadata(dependencyResource, 'server_only', 0) then
+                print(("^3%s is not a server-only resource, use dependency instead of server_dependency.^7"):format(dependencyResource))
+            end
+            if dependencyState ~= "started" and dependencyState ~= "starting" then
+                StartResource(dependencyResource)
+            end
+            ::nextIteration::
         end
-        local dependencyState = GetResourceState(dependencyResource)
-        if dependencyState == "missing" then
-            print(("Could not find server dependency %s for resource %s."):format(dependencyResource, resource))
-            CancelEvent()
-        elseif dependencyState ~= "started" and dependencyState ~= "starting" then
-            StartResource(dependencyResource)
-        end
-        ::nextIteration::
     end
 end)
